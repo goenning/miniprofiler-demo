50d2efdfa847c4ec303a1ec53dbfbd4504bdf8c7
goenning <oenning.ti@gmail.com>
2016-06-04 12:12:19 -0300

    dynamic port usage

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ app.get('/', function(req, res) {
   });
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
